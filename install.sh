50bf09698bca597ef1ba8e6fcd2afae982a5602f
Sergey Lungu <sergey.lungu@gmail.com>
2012-06-18 17:27:27 +0400

    [gtk] Added gtk3 config with Adwaita theme enabled.

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -Ts "$basedir/fonts/fonts.conf" ~/.fonts.conf
 ln -Ts "$basedir/git/gitconfig" ~/.gitconfig
 ln -Ts "$basedir/gtk/themes" ~/.themes
 ln -Ts "$basedir/gtk/gtkrc-2.0" ~/.gtkrc-2.0
+mkdir -p ~/.config/gtk-3.0 && ln -Ts "$basedir/gtk/gtkrc-3.0" ~/.config/gtk-3.0/settings.ini
 ln -Ts "$basedir/misc/bcrc" ~/.bcrc
 ln -Ts "$basedir/misc/filmtagrc" ~/.filmtagrc
 ln -Ts "$basedir/misc/inputrc" ~/.inputrc

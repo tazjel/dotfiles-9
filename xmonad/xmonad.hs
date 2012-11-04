e50d1dca021dc7cc1adc5ffccc86fa72478b2838
Sergey Lungu <sergey.lungu@gmail.com>
2012-11-04 21:10:30 +0400

    [xmonad] Use only one instance of xmobar. Refactoring.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,9 @@
+module Main where
+
 import XMonad
 
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 
@@ -16,68 +19,78 @@ import XMonad.Util.EZConfig
 
 import qualified XMonad.StackSet as W
 
-main = do
-    let defaultModMask = mod1Mask
+main :: IO ()
+main = xmonad $ ewmh $ defaultConfig
+    { modMask = mod1Mask
+    , terminal = "urxvtc"
+    , focusFollowsMouse = True
+    , normalBorderColor = "#6c6d6e"
+    , focusedBorderColor = "#dc6d6e"
+    , workspaces = ["1", "2", "3", "4", "5", "6"]
+    , layoutHook = myLayoutHook
+    , manageHook = myManageHook
+    , logHook = myLogHook =<< spawnPipe "xmobar ~/.xmobarrc"
+    } `additionalKeys` myKeys mod1Mask
 
-    let vertical = (Tall 1 (3/100) (60/100))
-    let horizontal = Mirror vertical
-    let full = Full
-    let gimp = withIM (1/6) ((ClassName "Gimp") `And` (Role "gimp-toolbox")) $ reflectHoriz $
-               withIM (1/6) ((ClassName "Gimp") `And` (Role "gimp-dock")) Full
+myLayoutHook = smartBorders . avoidStruts
+    $ onWorkspaces ["1", "2"] (vertical ||| horizontal ||| Full)
+    $ onWorkspaces ["3", "4", "6"] (Full ||| vertical ||| horizontal)
+    $ onWorkspace "5" (gimp ||| Full ||| vertical ||| horizontal)
+    $ layoutHook defaultConfig
+  where
+    vertical = (Tall 1 (3/100) (60/100))
+    horizontal = Mirror vertical
+    gimp = withIM (1/6) ((ClassName "Gimp") `And` (Role "gimp-toolbox"))
+         $ reflectHoriz
+         $ withIM (1/6) ((ClassName "Gimp") `And` (Role "gimp-dock")) Full
 
-    nScreens <- countScreens
-    xmobars <- mapM (\s -> spawnPipe ("xmobar -x " ++ show s ++ " ~/.xmonad/xmobarrc." ++ show s)) [0..nScreens-1]
-    xmonad $ defaultConfig
-        { modMask = defaultModMask
-        , terminal = "urxvtc"
-        , focusFollowsMouse = True
-        , normalBorderColor = "#6c6d6e"
-        , focusedBorderColor = "#dc6d6e"
-        , workspaces = ["1", "2", "3", "4", "5", "6"]
-        , manageHook = composeAll
-            [ isDialog --> doCenterFloat
-            , className =? "Google-chrome" --> doShift "3"
-            , className =? "Chrome" --> doShift "3"
-            , className =? "Deluge-gtk" --> doShift "3"
-            , className =? "Deluge" --> doShift "3"
-            , className =? "P4v.bin" --> doShift "4"
-            , className =? "Gthumb" --> doShift "4"
-            , className =? "Easytag" --> doShift "4"
-            , className =? "Inkscape" --> doShift "4"
-            , className =? "Gimp" --> doShift "5"
-            , (className =? "Gimp" <&&> (role =? "gimp-toolbox" <||> role =? "gimp-dock" <||> role =? "gimp-image-window")) --> unfloat
-            , className =? "VirtualBox" --> doShift "6"
-            , className =? "VBoxSDL" --> doShift "6"
-            , className =? "qemu-system-i386" --> doCenterFloat
-            , className =? "SWT" --> doCenterFloat
-            ] <+> manageDocks <+> manageHook defaultConfig
-        , layoutHook = smartBorders . avoidStruts
-            $ onWorkspaces ["1", "2"] (vertical ||| horizontal ||| full)
-            $ onWorkspaces ["3", "4", "6"] (full ||| vertical ||| horizontal)
-            $ onWorkspace "5" (gimp ||| full ||| vertical ||| horizontal)
-            $ layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-            { ppOutput = mapM_ (\(h, s) -> hPutStrLn h s) . zip xmobars . replicate 2
-            , ppCurrent = xmobarColor "#dcddde" "" 
-            , ppUrgent = xmobarColor "#8c0d0e" ""
-            , ppTitle = xmobarColor "#dcddde" "" . shorten 200
-            , ppSep = " :: "
-            , ppLayout = (\l -> case l of
-                "Tall" -> "V"
-                "Hinted Tall" -> "V"
-                "Mirror Tall" -> "H"
-                "Hinted Mirror Tall" -> "H"
-                "Full" -> "F"
-                "Hinted Full" -> "F"
-                "IM ReflectX IM Full" -> "G"
-                _ -> l)
-            }
-        } `additionalKeys`
-        [ ((defaultModMask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
-        , ((0, xK_Print), spawn "scrot ~/tmp/%Y%m%d-%H%M%S.png")
-        , ((defaultModMask, xK_Print), spawn "scrot -u ~/tmp/%Y%m%d-%H%M%S.png")
-        , ((defaultModMask, xK_p), spawn "exe=`dmenu_path | dmenu -nf '#acadae' -nb '#6c6d6e' -sf '#6c6d6e' -sb '#acadae' -fn -misc-fixed-medium-r-normal--14-130-75-75-c-70-*-*` && eval \"exec $exe\"")
-        ]
+myLogHook xmobar = dynamicLogWithPP xmobarPP
+    { ppOutput = hPutStrLn xmobar
+    , ppCurrent = xmobarColor "#dcddde" "" 
+    , ppUrgent = xmobarColor "#8c0d0e" ""
+    , ppTitle = xmobarColor "#dcddde" "" . shorten 200
+    , ppSep = " :: "
+    , ppLayout = (\l -> case l of
+        "Tall"                -> "V"
+        "Hinted Tall"         -> "V"
+        "Mirror Tall"         -> "H"
+        "Hinted Mirror Tall"  -> "H"
+        "Full"                -> "F"
+        "Hinted Full"         -> "F"
+        "IM ReflectX IM Full" -> "G"
+        _                     -> l)
+    }
+
+myManageHook :: ManageHook
+myManageHook = composeAll
+    [ isDialog                            --> doCenterFloat
+    , className  =? "Google-chrome"       --> doShift "3"
+    , className  =? "Chrome"              --> doShift "3"
+    , className  =? "Deluge-gtk"          --> doShift "3"
+    , className  =? "Deluge"              --> doShift "3"
+    , className  =? "P4v.bin"             --> doShift "4"
+    , className  =? "Gthumb"              --> doShift "4"
+    , className  =? "Easytag"             --> doShift "4"
+    , className  =? "Inkscape"            --> doShift "4"
+    , className  =? "Gimp"                --> doShift "5"
+    , (className =? "Gimp" <&&>
+           (role =? "gimp-toolbox"
+       <||> role =? "gimp-dock"
+       <||> role =? "gimp-image-window")) --> unfloat
+    , className  =? "VirtualBox"          --> doShift "6"
+    , className  =? "VBoxSDL"             --> doShift "6"
+    , className  =? "qemu-system-i386"    --> doCenterFloat
+    , className  =? "SWT"                 --> doCenterFloat
+    ] <+> manageDocks
+      <+> manageHook defaultConfig
   where
     unfloat = ask >>= doF . W.sink
     role = stringProperty "WM_WINDOW_ROLE"
+
+myKeys :: ButtonMask -> [((ButtonMask, KeySym), X ())]
+myKeys modMask =
+    [ ((modMask .|. shiftMask, xK_l), spawn "xscreensaver-command -lock")
+    , ((0, xK_Print), spawn "scrot ~/storage/tmp/%Y%m%d-%H%M%S.png")
+    , ((modMask, xK_Print), spawn "scrot -u ~/storage/tmp/%Y%m%d-%H%M%S.png")
+    , ((modMask, xK_p), spawn "exe=`dmenu_path | dmenu -nf '#acadae' -nb '#6c6d6e' -sf '#6c6d6e' -sb '#acadae' -fn -misc-fixed-medium-r-normal--14-130-75-75-c-70-*-*` && eval \"exec $exe\"")
+    ]
